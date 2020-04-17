5e7429dd41402c59491e4c7baedbfcb8068c6ef9
Roman Grubic <grubic.roman@gmail.com>
2020-04-17 08:58:47 +0000

    Added switchNameHandler to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, im a React developer!</h1>
-    </div>
-  );
+
+class App extends Component {
+    state = {
+        persons: [
+            { name: "Roman", age: 28},
+            { name: "Max", age: 29}
+        ],
+        otherState: "some other value"
+    }
+
+    switchNameHandler = () => {
+        // console.log('dd');
+        // dont do this this.state.persons[0].name = "Maximillian";
+        this.setState({
+            persons: [
+            { name: "Roman Grubic", age: 28},
+            { name: "Maximillian", age: 29}
+        ] })
+    }
+
+    render() {
+        return (
+            <div className="App">
+            <h1>Hi, im a React developer!</h1>
+            <button onClick={this.switchNameHandler}>Switch Name</button>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+            </div>
+        );
+    }
 }
-
 export default App;
