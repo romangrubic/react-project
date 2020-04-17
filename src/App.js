af0841b52582489e49cfe49118ca72f62999d8c1
Roman Grubic <grubic.roman@gmail.com>
2020-04-17 09:27:34 +0000

    Learning React hook useState()

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-class App extends Component {
-    state = {
+const App = (props) => {
+    const [personsState, setPersonsState] = useState({
         persons: [
             { name: "Roman", age: 28},
             { name: "Max", age: 29}
         ],
         otherState: "some other value"
-    }
+    });
 
-    switchNameHandler = () => {
+    const [otherState, setOtherState] = useState("some other value");
+    console.log(personsState, otherState);
+    const switchNameHandler = () => {
         // console.log('dd');
-        // dont do this this.state.persons[0].name = "Maximillian";
-        this.setState({
+        // dont do this personsState.persons[0].name = "Maximillian";
+    setPersonsState({
             persons: [
             { name: "Roman Grubic", age: 28},
             { name: "Maximillian", age: 29}
-        ] })
-    }
+        ],
+        otherState: personsState.otherState
+     });
+    };
 
-    render() {
-        return (
-            <div className="App">
-            <h1>Hi, im a React developer!</h1>
-            <button onClick={this.switchNameHandler}>Switch Name</button>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-            </div>
-        );
-    }
+    return (
+        <div className="App">
+        <h1>Hi, im a React developer!</h1>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+        </div>
+    );
 }
 export default App;
+
+
+
+
+    
