b8805d0cf11a08da32ff29b6e8ff340fe73a24b6
Roman Grubic <grubic.roman@gmail.com>
2020-04-17 17:22:19 +0000

    Added nameChangedHandler for person component

--- src/App.js
+++ src/App.js
@@ -16,18 +16,26 @@ class App extends Component {
     deletePersonHandler = (personIndex) => {
         const persons = [...this.state.persons];
         persons.splice(personIndex, 1);
-        this.setState({persons: persons});
+        this.setState({ persons: persons });
     }
 
-    nameChangedHandler = (event) => {
-        this.setState({
-            persons: [
-                { name: "Roman", age: 28 },
-                { name: event.target.value, age: 29 },
-                { name: 'Stephanie', age: 27 }
-            ]
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
         });
+
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState({ persons: persons })
     }
+
     togglePersonsHandler = () => {
         const doesShow = this.state.showPersons;
         this.setState({ showPersons: !doesShow });
@@ -48,10 +56,11 @@ class App extends Component {
                 <div>
                     { this.state.persons.map((person, index) => {
                         return <Person
-                            click={() => this.deletePersonHandler(index)}
+                            click={ () => this.deletePersonHandler(index) }
                             name={ person.name }
                             age={ person.age }
-                            key={person.id} />
+                            key={ person.id }
+                            changed={ (event) => this.nameChangedHandler(event, person.id) } />
                     }) }
                 </div>
             )
