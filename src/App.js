d2ecb84dd7ffd8c7616dc9d690a86c4e39f1f93f
Roman Grubic <grubic.roman@gmail.com>
2020-04-18 11:31:52 +0000

    Installed styled-components package for styling componennts.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
-import Radium, {StyleRoot} from 'radium';
+import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
+
+
+const StyledButton = styled.button`
+            background-color: ${props => props.alt ? 'red' : 'green'};
+            color:white;
+            font: inherit;
+            border: 1px solid blue;
+            padding: 8px;
+            cursor: pointer;
+
+            &:hover {
+                background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+                color: black;
+            }`;
 
 class App extends Component {
     state = {
@@ -43,20 +57,6 @@ class App extends Component {
     }
 
     render() {
-        const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black',
-            }
-        };
-
-
         let persons = null;
 
         if (this.state.showPersons) {
@@ -73,35 +73,34 @@ class App extends Component {
                 </div>
             );
             // setting styles dinamically
-            style.backgroundColor = 'red';
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                color: 'black'
-            };
+            // style.backgroundColor = 'red';
+            // style[':hover'] = {
+            //     backgroundColor: 'salmon',
+            //     color: 'black'
+            // };
         }
         // Dinamicly assigning classes 
         const classes = [];
-        if (this.state.persons.length <=2){
+        if (this.state.persons.length <= 2) {
             classes.push('red'); // classes= ['red']
         }
-        if (this.state.persons.length <= 1){
+        if (this.state.persons.length <= 1) {
             classes.push('bold'); // classes= ['red', 'bold']
         }
 
         return (
-            <StyleRoot>
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
-                <button
-                    style={ style }
-                    onClick={ this.togglePersonsHandler }>Toggle persons</button>
+                <p className={ classes.join(' ') }>This is really working!</p>
+                <StyledButton
+                    alt={this.state.showPersons}
+                    onClick={ this.togglePersonsHandler }>Toggle persons
+                </StyledButton>
                 { persons }
             </div>
-            </StyleRoot>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
 }
 
-export default Radium(App);
+export default App;
