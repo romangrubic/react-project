5040fcd31043f13747051d301a9b4245053f5a72
Roman Grubic <grubic.roman@gmail.com>
2020-04-19 12:51:30 +0000

    React highe order components.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
+// import classes from './Person.css';
 
 class Person extends Component {
     render(){
     return (
-        <div className={ classes.Person }>
-            <p onClick={ this.props.click }>Im { this.props.name } and I am { this.props.age } years old!</p>
-            <p>{ this.props.children }</p>
-            <input type="text" onChange={ this.props.changed } value={ this.props.name } />
-        </div>
+        <Aux>
+            <p key='i1' onClick={ this.props.click }>
+            Im { this.props.name } and I am { this.props.age } years old!
+            </p>
+            <p key='i2'>
+            { this.props.children }
+            </p>
+            <input 
+            key='i3' 
+            type="text" 
+            onChange={ this.props.changed } 
+            value={ this.props.name } />
+        </Aux>
     )
 }};
 
