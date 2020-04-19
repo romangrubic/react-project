79347306dd77058d14ee1b2a73e0fd4ee2dac14c
Roman Grubic <grubic.roman@gmail.com>
2020-04-19 13:14:15 +0000

    Some additional HOC.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxiliary'
 
 
 class App extends Component {
@@ -77,7 +79,7 @@ class App extends Component {
         };
 
         return (
-            <div className={classes.App}>  
+            <Aux>  
             <button onClick={() => {
                 this.setState({showCockpit: false});}}>Remove cockpit</button>
                 {this.state.showCockpit ? <Cockpit
@@ -86,9 +88,9 @@ class App extends Component {
                 personsLength={this.state.persons.length}
                 clicked={this.togglePersonsHandler} /> : null}             
                 { persons }
-            </div>
+            </Aux>
         );
     }
 }
 
-export default App;
+export default withClass(App, classes.App);
